0737e5498bc9a7e44bfdb0cb333f5d5f407d9008
RainbowBird <rbxin2003@outlook.com>
2021-08-26 01:23:44 +0800

    胡乱支持天气

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,3 +1,13 @@
 export default function Weather() {
-  return <div></div>;
+  return (
+    <div className={'box'}>
+      <iframe
+        src="https://weather-ten-alpha.vercel.app/weather.html?bg=3&md=034&lc=CN101240102&key=0b23abc522ab4c15804ace26ef307466"
+        frameBorder="0"
+        scrolling="no"
+        width="400"
+        height="790"
+      />
+    </div>
+  );
 }
